6fb414b78e25133bf245539d2c2f0823b3988c03
rj7002 <160825414+rj7002@users.noreply.github.com>
2024-09-14 14:08:22 -0700

    Update main.py

--- main.py
+++ main.py
@@ -442,9 +442,10 @@ def pass_map_3d(fig):
     periods = df_pass['period'].tolist()
     minutes = df_pass['minute'].tolist()
     seconds = df_pass['second'].tolist()
-
+    teams = df_pass['team_name'].tolist()
     # Determine the color based on the team
     color = 'blue' if menu_team == team_1 else 'red'
+    
     
     # Extract x and y coordinates
     x1 = np.array([el[0] for el in location])
@@ -479,6 +480,12 @@ def pass_map_3d(fig):
     #     y2=y2+10
     # Plot the passes as arrows
     for i in range(len(x1)):
+        team = teams[i]
+        if allteams:
+            if team == team_1:
+                color = 'blue'
+            else:
+                color = 'red'
         player = passer[i]
         reciever = recepient[i]
         passheight = passheightname[i].lower()
