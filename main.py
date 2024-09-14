9649b9adc5897a11dd598d5091fa321125e32496
rj7002 <160825414+rj7002@users.noreply.github.com>
2024-09-14 14:44:38 -0700

    Update main.py

--- main.py
+++ main.py
@@ -697,7 +697,7 @@ def pressure_map_3d(fig):
 def shot_map_3d(fig):
     # Filter the dataframe for shots by the selected player
     df_shot = df.loc[(df['type_name'] == 'Shot')]
-    st.write(df_shot)
+    # st.write(df_shot)
     location = df_shot['location'].tolist()
     shotend = df_shot['shot_end_location'].tolist()
     outcome = df_shot['shot_outcome_name'].tolist()
